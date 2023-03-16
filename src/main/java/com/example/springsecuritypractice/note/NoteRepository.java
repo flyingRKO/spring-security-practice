65f13c7b509ef6dd5c6a9eb298d8ccdf41f9aa44
Lee Jae Hun <wogns970@naver.com>
2023-03-17 03:35:51 +0900

    노트 서비스 로직 구현

---
+++ src/main/java/com/example/springsecuritypractice/note/NoteRepository.java
@@ -0,0 +1,14 @@
+package com.example.springsecuritypractice.note;
+
+
+import com.example.springsecuritypractice.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+    List<Note> findByUserOrderByIdDesc(User user);
+
+    Note findByIdAndUser(Long id, User user);
+}
